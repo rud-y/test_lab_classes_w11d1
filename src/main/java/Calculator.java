800723352741dc0a6484c8295a35d873eebcb2c9
rudy <rudolf.zachar@gmail.com>
2020-08-24 15:01:42 +0100

    canSubtract test passing

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -4,4 +4,7 @@ public class Calculator {
         return (num1 + num2);
     }
 
+    public int subtract(int num1, int num2) {
+        return (num1 - num2);
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -16,6 +16,11 @@ public class CalculatorTest {
         assertEquals(15, calculator.add(5, 10));
     }
 
+    @Test
+    public void canSubtract() {
+        assertEquals(10, calculator.subtract(25, 15));
+    }
+
 
 
 
