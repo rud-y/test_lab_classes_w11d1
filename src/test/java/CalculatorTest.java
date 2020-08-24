f53a8f86cf6f80a99ae56ea8272266e62b68c55e
rudy <rudolf.zachar@gmail.com>
2020-08-24 14:58:09 +0100

    Calculator class created, CalculatorTest created, first test canAdd() passing

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    Calculator calculator;
+
+    @Before
+    public void before() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void canAdd() {
+        assertEquals(15, calculator.add(5, 10));
+    }
+
+
+
+
+
+}
