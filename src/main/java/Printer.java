4ff4d179ef638b7f3e087ae61c06b7c2956606d3
rudy <rudolf.zachar@gmail.com>
2020-08-24 15:44:52 +0100

    Printer class created. hasPaper test pasing

---
+++ src/main/java/Printer.java
@@ -0,0 +1,16 @@
+public class Printer {
+    private int sheets;
+
+    public Printer(int sheets) {
+        this.sheets = sheets;
+    }
+
+    public int getSheets() {
+        return sheets;
+    }
+
+    public void setSheets(int sheets) {
+        this.sheets = sheets;
+    }
+
+}
---
+++ src/test/java/PrinterTest.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PrinterTest {
+
+    Printer printer;
+
+    @Before
+    public void before() {
+        printer = new Printer(100);
+    }
+
+    @Test
+    public void hasPaper() {
+        assertEquals(100, printer.getSheets());
+    }
+
+}
